f29805d9f6c6bd8cf787087e611d1a0028fcdda3
OlesiaKubska <kublesia0908@gmail.com>
2023-12-22 10:01:03 +0100

    add logic in the BaseModalWindow.jsx and styled in the BaseModalWindow.styled.jsx

--- src/components/common/BaseModalWindow/BaseModalWindow.jsx
+++ src/components/common/BaseModalWindow/BaseModalWindow.jsx
@@ -1,3 +1,50 @@
-export const BaseModalWindow = ({ children }) => {
-  return <div>BaseModalWindow{children}</div>;
+import { useEffect, useRef } from 'react';
+import { BaseModalStyled, CloseButton } from './BaseModalWindow.styled';
+
+export const BaseModalWindow = ({ children, title, onClose }) => {
+  useEffect(() => {
+    const handleEsc = e => {
+      if (e.code === 'Escape') {
+        onClose();
+      }
+    };
+
+    const handleClickOutside = event => {
+      if (!modalContainerRef.current.contains(event.target)) {
+        onClose();
+      }
+    };
+
+    window.addEventListener('keydown', handleEsc);
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      window.removeEventListener('keydown', handleEsc);
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [onClose]);
+
+  const modalContainerRef = useRef(null);
+
+  return (
+    <BaseModalStyled>
+      <div
+        className="modal-content"
+        onClick={e => e.stopPropagation()}
+        ref={modalContainerRef}
+      >
+        <div className="modal-header">
+          <h2>{title}</h2>
+          <CloseButton onClick={onClose}>
+            <a href="#">
+              <svg width="24" height="24">
+                <use href="/assets/images/sprite/symbol-defs.svg#icon-outline"></use>
+              </svg>
+            </a>
+          </CloseButton>
+        </div>
+        <div>{children}</div>
+      </div>
+    </BaseModalStyled>
+  );
 };
--- src/components/common/BaseModalWindow/BaseModalWindow.styled.jsx
+++ src/components/common/BaseModalWindow/BaseModalWindow.styled.jsx
@@ -1,1 +1,77 @@
 import styled from '@emotion/styled';
+
+export const BaseModalStyled = styled.div`
+  display: inline-flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-end;
+  position: fixed;
+  padding: 0;
+  margin: 0;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.8);
+  z-index: 1;
+
+  .modal-content {
+    transform: translate(-50%, -50%);
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: 100%;
+    background: ${props => props.theme.color.white};
+    border-radius: 10px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    gap: 24px;
+    padding: 32px 24px;
+    margin: 0;
+    transition: all ${props => props.theme.transition.modal};
+
+    @media (min-width: ${props => props.theme.breakpoint.mobile}) {
+      padding: 32px 12px;
+      width: calc(100% - 20px);
+    }
+
+    @media (min-width: ${props => props.theme.breakpoint.tablet}) {
+      width: 704px;
+    }
+  }
+
+  .modal-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    color: ${props => props.theme.color.black};
+
+    title {
+      font-weight: 500;
+      font-size: 26px;
+      line-height: 1.2;
+    }
+  }
+`;
+
+export const CloseButton = styled.button`
+  background: none;
+  width: 24px;
+  height: 24px;
+  padding: 0;
+
+  a {
+    stroke: ${props => props.theme.color.accent};
+    color: ${props => props.theme.color.accent};
+  }
+  svg {
+    width: 100%;
+    height: 100%;
+    background: yellow;
+    stroke: ${props => props.theme.color.accent};
+    fill: ${props => props.theme.color.accent};
+  }
+
+  /* &:hover {
+    color: ${props => props.theme.color.accent};
+  } */
+`;
