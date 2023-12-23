c7296bdce7a216c05750f0ef4ed1988cec145820
YanaGalayko <117485236+YanaGalayko@users.noreply.github.com>
2023-12-23 12:38:49 +0100

    add userLogo

--- src/components/SignIn/UserLogo/UserLogo.jsx
+++ src/components/SignIn/UserLogo/UserLogo.jsx
@@ -1,3 +1,24 @@
+import { useState } from 'react';
+import { UserLogoModal } from '../UserLogoModal/UserLogoModal';
+import sprite from 'src/assets/images/sprite/sprite.svg';
+import { UserLogoBtn, UserModalIcon } from './UserLogo.styled';
+
 export const UserLogo = () => {
-  return <div>UserLogo</div>;
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const showModal = () => {
+    setModalIsOpen(!modalIsOpen);
+  };
+
+  return (
+    <div>
+      <UserLogoBtn onClick={showModal}>
+        <p>user</p>
+        <UserModalIcon>
+          <use href={`${sprite}#icon-arrow-down`}></use>
+        </UserModalIcon>
+      </UserLogoBtn>
+      {modalIsOpen && <UserLogoModal onClose={showModal} />}
+    </div>
+  );
 };
--- src/components/SignIn/UserLogo/UserLogo.styled.jsx
+++ src/components/SignIn/UserLogo/UserLogo.styled.jsx
@@ -1,1 +1,19 @@
 import styled from '@emotion/styled';
+
+export const UserLogoBtn = styled.button`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+export const UserModalIcon = styled.svg`
+  width: 16px;
+  height: 16px;
+  stroke: ${({ theme }) => theme.color.accent};
+  fill: transparent;
+
+  &:hover,
+  &:focus {
+    color: ${props => props.theme.color.accent};
+  }
+`;
