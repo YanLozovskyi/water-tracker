174a6d92608963d5f525268a49fd63e236501128
anetta999 <124580023+anetta999@users.noreply.github.com>
2023-12-24 17:35:45 +0200

    fix userLogoModal and SettingModal components

--- src/components/SignIn/UserLogoModal/UserLogoModal.jsx
+++ src/components/SignIn/UserLogoModal/UserLogoModal.jsx
@@ -1,3 +1,49 @@
-export const UserLogoModal = () => {
-  return <div>UserLogoModal</div>;
+import { useState } from 'react';
+import sprite from 'src/assets/images/sprite/sprite.svg';
+import { SettingModal } from '../SettingModal/SettingModal';
+import {
+  IconSettings,
+  StyledBtn,
+  StyledListItem,
+  StyledLogoModal,
+} from './UserLogoModal.styled';
+
+export const UserLogoModal = ({ onClose }) => {
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  return (
+    <>
+      <div>
+        <StyledLogoModal>
+          <ul>
+            <StyledListItem>
+              <IconSettings width="16px" height="16px">
+                <use href={`${sprite}#icon-settings`}></use>
+              </IconSettings>
+              <StyledBtn type="button" onClick={openModal}>
+                Setting
+                {isModalOpen && <SettingModal onClose={closeModal} />}
+              </StyledBtn>
+            </StyledListItem>
+            <StyledListItem>
+              <IconSettings width="16px" height="16px">
+                <use href={`${sprite}#icon-exit`}></use>
+              </IconSettings>
+              <StyledBtn type="button" onClick={openModal}>
+                Log out
+              </StyledBtn>
+            </StyledListItem>
+          </ul>
+        </StyledLogoModal>
+      </div>
+    </>
+  );
 };
