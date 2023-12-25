7b7da8519c25b32bd07abbd7cb34e9c54483d528
YanLozovskyi <yanlozovskiy@gmail.com>
2023-12-25 14:08:51 +0200

    added two endpoints (updateAvatar & refresh user), processed them in authOperations and added selectors

---
+++ src/redux/auth/authSelectors.js
@@ -0,0 +1,4 @@
+export const selectUser = state => state.auth.user;
+export const selectUserToken = state => state.auth.token;
+export const selectIsLoggedIn = state => state.auth.isLoggedIn;
+export const selectIsRefreshing = state => state.auth.isRefreshing;
