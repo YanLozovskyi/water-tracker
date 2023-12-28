dd3305b588f9db82c382e5e6359713758a08f0ea
Ivano4ka8 <ivannamihalova@gmail.com>
2023-12-28 14:19:39 +0200

    add auth forms without styles for tablet and mobile

--- src/components/SignIn/SignInForm/SignInForm.jsx
+++ src/components/SignIn/SignInForm/SignInForm.jsx
@@ -1,7 +1,154 @@
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import sprite from 'src/assets/images/sprite/sprite.svg';
+import {
+  BootleImg,
+  ErrorSpan,
+  EyeSlashIcon,
+  FormTitle,
+  SignButton,
+  SignButtonDisabled,
+  SignForm,
+  SignStyledInput,
+  SignStyledInputError,
+  SignStyledLabel,
+  SignUpContainer,
+  SignUpLink,
+} from '../../SignUp/SignUpForm/SignUpForm.styled';
+import { logInThunk } from '../../../redux/auth/authOperations';
+import { useNavigate } from 'react-router-dom';
+import { SignInLink } from './SignInForm.styled';
+
 export const SignInForm = () => {
+  const dispatch = useDispatch();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [iconStatus, setIconStatus] = useState(false);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    validatePassword(password);
+  }, [password]);
+
+  const validateEmail = () => {
+    const emailRegex = /^[^\s@]+@[^\s@]+.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      setEmailError('please enter a valid email address');
+    } else {
+      setEmailError('');
+    }
+  };
+
+  const validatePassword = password => {
+    if (password && password.length < 8) {
+      setPasswordError('Password must be at least 8 characters long');
+    } else {
+      setPasswordError('');
+    }
+  };
+
+  const handleChange = ({ target: { name, value } }) => {
+    switch (name) {
+      case 'email':
+        setEmail(value);
+        validateEmail();
+        return;
+      case 'password':
+        setPassword(value);
+        validatePassword();
+        return;
+      default:
+        return;
+    }
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    dispatch(logInThunk({ email, password })).then(data => {
+      if (!data.error) navigate('/home');
+    });
+    setEmail('');
+    setPassword('');
+  };
+
+  const iconClick = e => {
+    iconStatus ? setIconStatus(false) : setIconStatus(true);
+  };
+
+  const buttonDisabled = emailError || passwordError;
+
   return (
-    <>
-      <div>SignInForm</div>
-    </>
+    <SignUpContainer>
+      <BootleImg />
+      <SignForm onSubmit={handleSubmit}>
+        <FormTitle>Sign In</FormTitle>
+        <SignStyledLabel>
+          Enter your email
+          {emailError ? (
+            <SignStyledInputError
+              onChange={handleChange}
+              placeholder="E-mail"
+              type="email"
+              name="email"
+              value={email}
+            />
+          ) : (
+            <SignStyledInput
+              onChange={handleChange}
+              placeholder="E-mail"
+              type="email"
+              name="email"
+              value={email}
+            />
+          )}
+          {emailError && <ErrorSpan>{emailError}</ErrorSpan>}
+        </SignStyledLabel>
+        <SignStyledLabel>
+          Enter your password
+          {passwordError ? (
+            <SignStyledInputError
+              onChange={handleChange}
+              autoComplete="off"
+              type={iconStatus ? 'text' : 'password'}
+              placeholder="Password"
+              name="password"
+              value={password}
+            />
+          ) : (
+            <SignStyledInput
+              onChange={handleChange}
+              autoComplete="off"
+              type={iconStatus ? 'text' : 'password'}
+              placeholder="Password"
+              name="password"
+              value={password}
+            />
+          )}
+          {passwordError && <ErrorSpan>{passwordError}</ErrorSpan>}
+          {iconStatus === false ? (
+            <EyeSlashIcon onClick={iconClick}>
+              <use href={`${sprite}#icon-to-hide`}></use>
+            </EyeSlashIcon>
+          ) : (
+            <EyeSlashIcon onClick={iconClick}>
+              <use href={`${sprite}#icon-to-open`}></use>
+            </EyeSlashIcon>
+          )}
+        </SignStyledLabel>
+        {buttonDisabled ? (
+          <SignButtonDisabled type="submit" disabled={buttonDisabled}>
+            SignIn
+          </SignButtonDisabled>
+        ) : (
+          <SignButton type="submit" disabled={buttonDisabled}>
+            SignIn
+          </SignButton>
+        )}
+        <SignUpLink to="/signup">Sign up</SignUpLink>
+      </SignForm>
+    </SignUpContainer>
   );
 };
--- src/components/SignUp/SignUpForm/SignUpForm.styled.jsx
+++ src/components/SignUp/SignUpForm/SignUpForm.styled.jsx
@@ -1,49 +1,87 @@
 import styled from '@emotion/styled';
 import { NavLink } from 'react-router-dom';
+import imgHome2Mob1x from '../../../assets/images/background/homePage/mobile/bottleMob.png';
+import imgHome2Mob2x from '../../../assets/images/background/homePage/mobile/bottleMob@2x.png';
+import imgHome2Tabl1x from '../../../assets/images/background/homePage/tablet/bottleTabl.png';
+import imgHome2Tabl2x from '../../../assets/images/background/homePage/tablet/bottleTabl@2x.png';
+import imgHome2Desk1x from '../../../assets/images/background/homePage/desktop/bottleDesc.png';
+import imgHome2Desk2x from '../../../assets/images/background/homePage/desktop/bottleDesc@2x.png';
 
-export const SignImgBubble = styled.img`
-  position: absolute;
-  background-size: cover;
-  background-position: center;
-  width: 1404px;
-  height: 582px;
-  pointer-events: none;
-  @media (max-width: 768px) {
-    width: 768px;
-    height: 680px;
-  }
-  @media (max-width: 480px) {
-    width: 320px;
-    height: 842px;
-    display: inline-flex;
-    padding-bottom: 2.302px;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-  }
-`;
+// export const SignImgBubble = styled.img`
+//   position: absolute;
+//   background-size: cover;
+//   background-position: center;
+//   width: 1404px;
+//   height: 582px;
+//   pointer-events: none;
+//   @media (max-width: 768px) {
+//     width: 768px;
+//     height: 680px;
+//   }
+//   @media (max-width: 480px) {
+//     width: 320px;
+//     height: 842px;
+//     display: inline-flex;
+//     padding-bottom: 2.302px;
+//     flex-direction: column;
+//     justify-content: center;
+//     align-items: center;
+//   }
+// `;
 
-export const SignImgButle = styled.img`
-  position: absolute;
+// export const SignImgButle = styled.img`
+//   position: absolute;
+//   width: 916px;
+//   height: 680px;
+//   pointer-events: none;
+//   @media (max-width: 768px) {
+//     width: 736px;
+//     height: 548px;
+//     margin-top: 52px;
+//     margin-left: 112px;
+//   }
+//   @media (max-width: 480px) {
+//     width: 280px;
+//     height: 210px;
+//     margin-top: 394px;
+//     margin-left: 20px;
+//   }
+// `;
+
+export const BootleImg = styled.div`
+  background-position: -120px center;
+  background-image: -webkit-image-set(
+    url(${imgHome2Mob1x}) 1x,
+    url(${imgHome2Mob2x}) 2x
+  );
+  background-size: contain;
+  background-repeat: no-repeat;
   width: 916px;
-  height: 680px;
-  pointer-events: none;
-  @media (max-width: 768px) {
-    width: 736px;
-    height: 548px;
-    margin-top: 52px;
-    margin-left: 112px;
+  min-height: 680px;
+
+  @media screen and (min-width: ${({ theme }) => theme.breakpoint.tablet}) {
+    background-image: -webkit-image-set(
+      url(${imgHome2Tabl1x}) 1x,
+      url(${imgHome2Tabl2x}) 2x
+    );
+    height: 386px;
   }
-  @media (max-width: 480px) {
-    width: 280px;
-    height: 210px;
-    margin-top: 394px;
-    margin-left: 20px;
+
+  @media screen and (min-width: ${({ theme }) => theme.breakpoint.desktop}) {
+    background-image: -webkit-image-set(
+      url(${imgHome2Desk1x}) 1x,
+      url(${imgHome2Desk2x}) 2x
+    );
+    width: 916px;
+    min-height: 680px;
   }
 `;
 
 export const SignUpContainer = styled.div`
-  width: 1404px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  /* width: 1404px;
   height: 582px;
   top: 52px;
   left: 18px;
@@ -79,47 +117,47 @@ export const SignUpContainer = styled.div`
     }
   }
 
-  .sign-up-forms-container h2 {
-    margin-bottom: 16px;
-    width: 100%;
-    font-size: 26px;
-    font-style: normal;
-    font-weight: 500;
-    line-height: 32px;
-    color: ${props => props.theme.color.black};
-  }
-
-  .sign-up-button {
-    width: 100%;
-    padding: 10px 30px 10px 30px;
-    display: flex;
-    justify-content: center;
-    border-radius: 10px;
-    box-shadow: 0px 4px 8px 0px rgba(64, 123, 255, 0.34);
-    text-align: center;
-    color: ${props => props.theme.color.white};
-    background-color: ${props => props.theme.color.accent};
-    margin-bottom: 16px;
-  }
-
-  .sign-up-button-disabled {
-    background-color: ${props => props.theme.color.secondaryPowderBlue};
-  }
-  
   .input-with-error {
-    color: red;
-    border: 1px solid ${props => props.theme.color.secondaryRed};
+  
   }
-  .sign-up-error-message {
-    color: red;
-  }
+  */
+`;
+
+export const ErrorSpan = styled.span`
+  color: red;
+`;
+
+export const SignButton = styled.button`
+  width: 100%;
+  padding: 10px 30px 10px 30px;
+  display: flex;
+  justify-content: center;
+  border-radius: 10px;
+  box-shadow: 0px 4px 8px 0px rgba(64, 123, 255, 0.34);
+  text-align: center;
+  color: ${props => props.theme.color.white};
+  background-color: ${props => props.theme.color.accent};
+`;
+
+export const SignButtonDisabled = styled(SignButton)`
+  background-color: ${props => props.theme.color.secondaryPowderBlue};
+  cursor: not-allowed;
+`;
+
+export const FormTitle = styled.h2`
+  width: 100%;
+  font-size: 26px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 32px;
+  color: ${props => props.theme.color.black};
 `;
 export const SignStyledLabel = styled.label`
   display: flex;
   flex-direction: column;
   position: relative;
   width: 100%;
-  margin-bottom: 8px;
+  gap: 8px;
   font-size: 18px;
   font-style: normal;
   font-weight: 400;
@@ -136,7 +174,6 @@ export const SignStyledInput = styled.input`
   justify-content: center;
   align-items: flex-start;
   gap: 10px;
-  margin-bottom: 16px;
 
   &:focus-within {
     outline: none;
@@ -144,6 +181,19 @@ export const SignStyledInput = styled.input`
   &::placeholder {
     color: ${props => props.theme.color.secondaryLightBlue};
   }
+`;
+
+export const SignStyledInputError = styled(SignStyledInput)`
+  color: red;
+  border: 1px solid ${props => props.theme.color.secondaryRed};
+`;
+
+export const SignForm = styled.form`
+  min-width: 384px;
+  min-height: 312px;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
 `;
 
 export const EyeSlashIcon = styled.svg`
@@ -157,14 +207,14 @@ export const EyeSlashIcon = styled.svg`
   top: 38px;
 `;
 
-export const SignUpLink = styled(NavLink)` 
+export const SignUpLink = styled(NavLink)`
   width: 100%;
   font-size: 16px;
-  font-style: normal; 
+  font-style: normal;
   font-weight: 400;
   line-height: 20px;
   color: ${props => props.theme.color.accent};
-    &:hover {
+  &:hover {
     color: ${props => props.theme.color.secondaryYellow};
-  }`
-
+  }
+`;
