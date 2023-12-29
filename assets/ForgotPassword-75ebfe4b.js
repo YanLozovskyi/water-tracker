dc53ab6509dfdc191c20b2c708009544a4808be1
YanLozovskyi <118999406+YanLozovskyi@users.noreply.github.com>
2023-12-29 16:55:03 +0000

    Deploying to gh-pages from @ YanLozovskyi/water-tracker@3f760dacfaa4e6c8dfe56d022e91bcb792a5d527 🚀

--- assets/ForgotPassword-75ebfe4b.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-74abdfda.js";import{S as r}from"./Section-ba165e2a.js";const s=()=>o.jsx("div",{children:"ForgotPasswordForm"}),n=()=>o.jsxs(r,{children:["  ",o.jsx(s,{})]});export{n as default};
