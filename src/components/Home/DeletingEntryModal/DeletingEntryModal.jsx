cfb4bfee17bd7382e63a761a7732c54da0aa7e08
Your Name <your-email@example.com>
2024-01-03 16:03:16 +0200

    add styles

--- src/components/Home/DeletingEntryModal/DeletingEntryModal.jsx
+++ src/components/Home/DeletingEntryModal/DeletingEntryModal.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import { BaseModalWindow } from 'components';
+import {
+  ButtonStyle,
+  ModalContainer,
+  TextStyle,
+  ButtonBox,
+} from './DeletingEntryModal.styled';
 
 export const DeletingEntryModal = ({ onClose, onDelete, title }) => {
   return (
     <BaseModalWindow onClose={onClose} title={title}>
-      <p>Are you sure you want to delete the entry?</p>
-      <button onClick={onClose}>Cancel</button>
-      <button onClick={onDelete}>Delete</button>
+      <ModalContainer>
+        <TextStyle>Are you sure you want to delete the entry?</TextStyle>
+        <ButtonBox>
+          <ButtonStyle onClick={onDelete}>Delete</ButtonStyle>
+          <ButtonStyle onClick={onClose}>Cancel</ButtonStyle>
+        </ButtonBox>
+      </ModalContainer>
     </BaseModalWindow>
   );
 };
