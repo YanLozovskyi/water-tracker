bbd49738fa70117ace69cfc8db5be3c33b550a40
YanLozovskyi <118999406+YanLozovskyi@users.noreply.github.com>
2024-01-09 10:36:13 +0000

    Deploying to gh-pages from @ YanLozovskyi/water-tracker@c509154d6d205ec8f8a517e1cf404e5394bb21bd 🚀

---
+++ assets/Error-9f18b05a.js
@@ -0,0 +1,3 @@
+import{r,j as t,aL as n,aM as e,aN as s,n as a}from"./index-2c083c9e.js";import{S as c}from"./Section-819b955e.js";const i="/assets/notFound-5bfb905e.png",d=2e4,u=()=>(r.useEffect(()=>{const o=setTimeout(()=>{window.location.href="/"},d);return()=>clearTimeout(o)},[]),t.jsxs(t.Fragment,{children:[t.jsx(n,{to:"/",children:t.jsx(e,{type:"button",children:"Go back to Welcome page"})}),t.jsx(s,{src:i,alt:"notFound"})]})),m=a(c)`
+padding-top: 30px;
+`,l=()=>t.jsx(m,{children:t.jsx(u,{})});export{l as default};
--- assets/SignUp-8d17fe2f.js
+++ assets/SignUp-8d17fe2f.js
@@ -1,17 +1,17 @@
-import{n as a,j as i,aJ as n}from"./index-885c0822.js";import{S as o}from"./Section-3bd3e10b.js";import{i as t,a as r,b as m,c as s,d as g,e as d}from"./bubbleTablet@2x-6c947164.js";const x=a(o)`
+import{n as a,j as i,aK as o}from"./index-2c083c9e.js";import{S as t}from"./Section-819b955e.js";import{i as r,a as m,b as n,c as s,d as g,e as d}from"./bubbleTablet@2x-6c947164.js";const p=a(t)`
   padding-top: 40px;
-  background-position: center center;
+  background-position: top;
   background-image: -webkit-image-set(
-    url(${t}) 1x,
-    url(${r}) 2x
+    url(${r}) 1x,
+    url(${m}) 2x
   );
-  background-size: contain;
+  background-size: cover;
   background-repeat: no-repeat;
   width: 100%;
 
   @media screen and (min-width: ${({theme:e})=>e.breakpoint.tablet}) {
     background-image: -webkit-image-set(
-      url(${m}) 1x,
+      url(${n}) 1x,
       url(${s}) 2x
     );
   }
@@ -22,4 +22,4 @@ import{n as a,j as i,aJ as n}from"./index-885c0822.js";import{S as o}from"./Sect
       url(${d}) 2x
     );
   }
-`,k=()=>i.jsx(x,{children:i.jsx(n,{})});export{k as default};
+`,u=()=>i.jsx(p,{children:i.jsx(o,{})});export{u as default};
